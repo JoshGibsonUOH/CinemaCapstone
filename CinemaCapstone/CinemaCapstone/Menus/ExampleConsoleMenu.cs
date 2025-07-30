10b306fbef6377118d46dd7cc7f82ddaa0449d8a
JoshGibsonUOH <119119908+JoshGibsonUOH@users.noreply.github.com>
2025-07-30 10:11:59 +0100

    Add files via upload
    Uploaded finished assignment

---
+++ CinemaCapstone/CinemaCapstone/Menus/ExampleConsoleMenu.cs
@@ -0,0 +1,41 @@
+﻿namespace Capstone.Menus
+{
+	/// <summary>
+	/// Represents an example console menu that inherits from <see cref="ConsoleMenu"/>.
+	/// This menu allows users to add numbers to a running total stored in an instance of the ExampleSumObject business class.
+	/// </summary>
+	internal class ExampleConsoleMenu : ConsoleMenu
+	{
+		private ExampleSumObject _totalSum;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ExampleConsoleMenu"/> class with a specified <see cref="ExampleSumObject"/>.
+		/// </summary>
+		/// <param name="sum">An instance of <see cref="ExampleSumObject"/> that maintains the running total.</param>
+		public ExampleConsoleMenu(ExampleSumObject sum)
+		{
+			_totalSum = sum;
+		}
+
+		/// <summary>
+		/// Creates the menu by adding menu items.
+		/// </summary>
+		public override void CreateMenu()
+		{
+			_menuItems.Clear();
+			_menuItems.Add(new ExampleMenuItem(ref _totalSum, 1, 10));
+			_menuItems.Add(new ExampleMenuItem(ref _totalSum, 21, 30));
+			_menuItems.Add(new ExampleMenuItem(ref _totalSum, 100, 150));
+			_menuItems.Add(new ExitMenuItem(this));
+		}
+
+		/// <summary>
+		/// Gets the text to be displayed for the console menu.
+		/// </summary>
+		/// <returns>A string representing the menu text and the current total sum.</returns>
+		public override string MenuText()
+		{
+			return $"Example Menu That Adds Numbers {Environment.NewLine}Total : {_totalSum.Total}";
+		}
+	}
+}
---
+++ CinemaCapstone/CinemaCapstone/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Capstone;
+using Capstone.Menus;
+
+Console.WriteLine("Hello, Capstone!");
+
+/*
+ * This program is provided as a starting point to the capstone project.
+ * It shows an example of how you can create business objects (in this case an instance of ExampleSumObject and
+ * interact with them using the ConsoleMenu and MenuItem objects.
+ * 
+ * You do not have to use this menuing system for the capstone project if you don't want to, but if you do this 
+ * might provide a good starting point.
+ * 
+ */
+
+ExampleSumObject mySum = new ExampleSumObject();
+
+ExampleConsoleMenu myMenu = new ExampleConsoleMenu(mySum);
+myMenu.Select();
+
+string[] options = { "option 1", "option 2", "option 3" };
+
+int selection = ConsoleHelpers.GetSelectionFromMenu(options, "Please select an option");
+Console.WriteLine($"{options[selection]} was selected.");
+
+string path = $@"{Environment.CurrentDirectory}\Resources";
+string[] resourceFiles = Directory.GetFiles(path, "*.txt");
+
+selection = ConsoleHelpers.GetSelectionFromMenu(resourceFiles, "Select a file to load");
+
+string[] fileLines = File.ReadAllLines(resourceFiles[selection]);
+
+foreach(string line in fileLines)
+{
+	Console.WriteLine(line);
+}
